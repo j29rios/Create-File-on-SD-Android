a595d5375688edcedb94299c66eb617e436835a3
jorge <jorge@gmail.com>
2015-03-06 00:30:39 -0500

    1.0

---
+++ app/src/main/java/com/jorgerios/files/MainActivity.java
@@ -0,0 +1,62 @@
+package com.jorgerios.files;
+
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FileManager fm = new FileManager("file.txt");
+        if(fm.sdDisponible()){
+            for(int i = 0; i < 5; i++) {
+                fm.writeFile("Linea " + i);
+            }
+        }
+
+        String[] array = fm.fileToArray();
+
+        for(int j = 0; j < array.length; j++){
+            Log.e("Arreglo", array[j]);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
